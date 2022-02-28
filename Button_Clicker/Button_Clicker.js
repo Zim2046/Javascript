be12fea90d6616cc01091f0a014bb2b5c09a96c0
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-02-27 20:08:49 -0800

    Initial Commit

---
+++ Button_Clicker/Button_Clicker.js
@@ -0,0 +1,12 @@
+function logOut(element) {
+    element.innerText = "Log Out";
+}
+
+function removeElement(element){
+    element.remove()
+}
+
+function notify(){
+    alert("Ninja was liked")
+}
+

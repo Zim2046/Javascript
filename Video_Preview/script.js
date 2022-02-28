4aa00e8ea6fbf4384cb082f67c231ff71b3c7431
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-02-27 22:00:06 -0800

    Initial Commit

---
+++ Video_Preview/script.js
@@ -0,0 +1,14 @@
+console.log("page loaded...");
+
+function playVid(element){
+    element.play()
+    element.muted = true
+}
+function pauseVid(element){
+    element.pause()
+    element.muted = false
+
+}
+function unMuteReadyPlay(element){
+    element.muted = false
+}

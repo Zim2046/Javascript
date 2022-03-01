f7f051d43b79a495443e37c193296fcf49791b19
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-03-01 12:30:46 -0800

    Initial Commit

---
+++ Making_Connections/Profile_Page_Connections.js
@@ -0,0 +1,41 @@
+
+
+var requests = 2;
+var connections = 418;
+var newName = "Timothy Thomas";
+// var countPhil = 12;
+var connectElement = document.querySelector("#connections")
+var requestElement = document.querySelector("#requests")
+var profileNameElement = document.querySelector("#userName")
+
+function addToListTodd(countElement,requestElement){
+    connections++
+    requests--
+    countElement.innerText = connections;
+    requestElement.innerText = requests;
+    document.querySelector("#todd").remove();
+}
+function addToListPhil(countElement,requestElement){
+    connections++
+    requests--
+    countElement.innerText = connections;
+    requestElement.innerText = requests;
+    document.querySelector("#phil").remove();
+}
+function removeTodd(requestElement){
+    requests--
+    requestElement.innerText = requests;
+    document.querySelector("#todd").remove();
+}
+function removePhil(requestElement){
+    requests--
+    requestElement.innerText = requests;
+    document.querySelector("#phil").remove();
+}
+function changeProfileName(profileNameElement){
+    profileNameElement.innerText = newName;
+}
+
+
+
+

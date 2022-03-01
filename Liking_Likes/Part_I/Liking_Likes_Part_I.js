a2d9e81ee45d98cdea1809b77f4dfb1c94cbebcc
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-02-28 18:07:19 -0800

    Initial Commit

--- Liking_Likes/Part_I/Liking_Likes_Part_I.js
+++ Liking_Likes/Part_I/Liking_Likes_Part_I.js
@@ -1,8 +1,7 @@
-var count =3; // initial likes
+var count = 3; // initial likes
 var countElement = document.querySelector(".top p")
 
 function addALike(countElement){
     count++
-
     countElement.innerText = count + " Like(s)";
 }
--- Liking_Likes/Part_II/Liking_Likes_Part_II.js
+++ Liking_Likes/Part_II/Liking_Likes_Part_II.js
@@ -3,28 +3,22 @@ var likesArray = [{ "countNiel": 9},{"countNichole": 12},{"countJim" :9}]
 
 // var countNiel = 9;
 var countNielElement = document.querySelector("#Niel p")
-
 // var countNichole = 12;
 var countNicholeElement = document.querySelector("#Nichole p")
-
 // var countJim = 9;
 var countJimElement = document.querySelector("#Jim p")
 
-
 function addALikeNiel(countElement){
     likesArray[0].countNiel++
-
     countElement.innerText = likesArray[0].countNiel + " Like(s)";
 }
 
 function addALikeNichole(countElement){
     likesArray[1].countNichole++
-
     countElement.innerText = likesArray[1].countNichole + " Like(s)";
 }
 
 function addALikeJim(countElement){
     likesArray[2].countJim++
-
     countElement.innerText = likesArray[2].countJim + " Like(s)";
 }

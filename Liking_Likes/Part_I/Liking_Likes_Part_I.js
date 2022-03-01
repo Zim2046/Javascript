f04af330d4fa80d3ad22f8b89f2138a347e3594b
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-02-28 18:01:18 -0800

    Initial Commit

---
+++ Liking_Likes/Part_I/Liking_Likes_Part_I.js
@@ -0,0 +1,8 @@
+var count =3; // initial likes
+var countElement = document.querySelector(".top p")
+
+function addALike(countElement){
+    count++
+
+    countElement.innerText = count + " Like(s)";
+}

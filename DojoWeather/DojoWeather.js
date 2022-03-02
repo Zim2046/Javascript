0eda6430ac449c210b339bdd27176bfb795c89d2
Zim2046 <99115132+Zim2046@users.noreply.github.com>
2022-03-01 23:41:45 -0800

    added C to F converter, but with compunding error for every new selection

--- DojoWeather/DojoWeather.js
+++ DojoWeather/DojoWeather.js
@@ -11,28 +11,43 @@ function removeCookies(element){
 
 //todo OnSelect functions  
 var tempValuesHigh = document.querySelectorAll('.high');
-console.log(tempValuesHigh[0].innerText)
+// console.log(tempValuesHigh[0].innerText)
 
 var tempValuesLow = document.querySelectorAll('.low');
-console.log(tempValuesLow[0].innerText)
+// console.log(tempValuesLow[0].innerText)
+// console.log(parseInt(tempValuesLow[0].innerText))
 
 function tempConvert(element){
     
-    if(element.value == '°C'){
+    if(element.value != '°C'){
         console.log("The units  are: ",element.value);
         for (var index = 0; index < tempValuesHigh.length; index++) {
-            const currentHighTemp = tempValuesHigh[index] * (9/5)+32;
-            
+            const currentHighTemp = parseInt(tempValuesHigh[index].innerText)  * (9/5)+32;
+            var truncatedVal_H_C2F = Math.trunc(currentHighTemp)
+            console.log(truncatedVal_H_C2F)
+            tempValuesHigh[index].innerText = truncatedVal_H_C2F
+        }
+        for (var index = 0; index < tempValuesLow.length; index++) {
+            const currentLowTemp = parseInt(tempValuesLow[index].innerText)  * (9/5)+32;
+            var truncatedVal_L_C2F = Math.trunc(currentLowTemp)
+            console.log(truncatedVal_L_C2F)
+            tempValuesLow[index].innerText = truncatedVal_L_C2F
         }
     }
     else{
         console.log("The units  are: ",element.value);
-
+        for (var index = 0; index < tempValuesHigh.length; index++) {
+            const currentHighTemp = (parseInt(tempValuesHigh[index].innerText) - 32)  * 5/9;
+            var truncatedVal_H_F2C = Math.trunc(currentHighTemp)
+            console.log(truncatedVal_H_F2C)
+            tempValuesHigh[index].innerText = truncatedVal_H_F2C
+        }
+        for (var index = 0; index < tempValuesLow.length; index++) {
+            const currentLowTemp = (parseInt(tempValuesLow[index].innerText) - 32)  * 5/9;
+            var truncatedVal_LF2C = Math.trunc(currentLowTemp)
+            console.log(truncatedVal_LF2C)
+            tempValuesLow[index].innerText = truncatedVal_LF2C
+        }
     }
-    // updates and stores the newly-selected foodToOrder variable
-    tempUnits = element.value;
-}
-// 👇🏼alerts the updated foodToOrder variable
-function orderFood(){
-    alert("Ordering a "+ foodToOrder)
+
 }
